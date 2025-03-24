62115471fe1efae56d18f69212133df24f9aa277
Hanzhong Liu <liuhanzhong.lhz@gmail.com>
2025-03-24 13:57:00 -0500

    add contactMatrix demo

--- app/routes_web.py
+++ app/routes_web.py
@@ -3,3 +3,8 @@ from flask import Flask, jsonify, render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/matrix')
+def matrix():
+    return render_template('matrix.html')
